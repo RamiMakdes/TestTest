10c30c13fad1149688a9efde221f542ef3255f89
RamiMakdes <ramimakdes@gmail.com>
2024-02-01 12:24:11 +0100

    Initial Commit

---
+++ TestTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestTest
+//
+//  Created by Rami Makdes on 2024-02-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestTest/TestTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTestApp.swift
+//  TestTest
+//
+//  Created by Rami Makdes on 2024-02-01.
+//
+
+import SwiftUI
+
+@main
+struct TestTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
